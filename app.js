16bc98dd97824b52f0d187463bdc2935007e1304
Kevin-HA-KHA <101355967+Kevin-HA-KHA@users.noreply.github.com>
2023-11-02 12:27:51 +0100

    Update app.js

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ function getError(errorMessage) {
 //API call function
 function getWeather(latitude, longitude) {
     const key = "82005d27a116c2880c8f0fcb866998a0";
-    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}&lang=fr`;
+    let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}&lang=fr`;
     let data;
     fetch(api)
         .then((response) => {
